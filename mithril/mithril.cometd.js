4fd0c547cb9b437c8aa32919d1e9c40585a5a564
Jón Atli Ólafsson <jonatli933@gmail.com>
2018-04-06 15:06:23 +0000

    attempt to deserialize the response instead of just JSON.parse

--- mithril/mithril.cometd.js
+++ mithril/mithril.cometd.js
@@ -1,73 +1,89 @@
-var m = require('mithril');
-var org_cometd = require('../org/cometd');
+var m = require("mithril");
+var org_cometd = require("../org/cometd");
 
 org_cometd.JSON.toJSON = JSON.stringify;
 org_cometd.JSON.fromJSON = JSON.parse;
 
 function _setHeaders(xhr, headers) {
-    if (headers) {
-        Object.keys(headers).forEach(function(name){
-            if (name.toLowerCase() === 'content-type') {
-                return;
-            }
-            xhr.setRequestHeader(name, headers[name]);
-        });
-    }
+  if (headers) {
+    Object.keys(headers).forEach(function(name) {
+      if (name.toLowerCase() === "content-type") {
+        return;
+      }
+      xhr.setRequestHeader(name, headers[name]);
+    });
+  }
 }
 
 function LongPollingTransport() {
-    var _super = new org_cometd.LongPollingTransport();
-    var that = org_cometd.Transport.derive(_super);
+  var _super = new org_cometd.LongPollingTransport();
+  var that = org_cometd.Transport.derive(_super);
 
-    that.xhrSend = function (packet) {
-        return m.request({
-            url: packet.url,
-            method: 'POST',
-            data: packet.body,
-            config: function (xhr) {
-                xhr.withCredentials = true;
-                xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-                _setHeaders(xhr, packet.headers);
-            }
-        }).then(packet.onSuccess, packet.onError);
-    };
+  that.xhrSend = function(packet) {
+    return m
+      .request({
+        url: packet.url,
+        method: "POST",
+        data: packet.body,
+        deserialize: attempt(JSON.parse),
+        config: function(xhr) {
+          xhr.withCredentials = true;
+          xhr.setRequestHeader(
+            "Content-Type",
+            "application/json;charset=UTF-8"
+          );
+          _setHeaders(xhr, packet.headers);
+        }
+      })
+      .then(packet.onSuccess, packet.onError);
+  };
 
-    return that;
+  return that;
 }
 
 function CallbackPollingTransport() {
-    var _super = new org_cometd.CallbackPollingTransport();
-    var that = org_cometd.Transport.derive(_super);
+  var _super = new org_cometd.CallbackPollingTransport();
+  var that = org_cometd.Transport.derive(_super);
 
-    that.jsonpSend = function (packet) {
-        m.request({
-            url: packet.url,
-            method: 'GET',
-            dataType: 'jsonp',
-            callbackKey: 'jsonp',
-            data: {
-                message: packet.body
-            },
-            config: function (xhr) {
-                _setHeaders(xhr, packet.headers);
-            }
-        }).then(packet.onSuccess, packet.onError);
-    };
+  that.jsonpSend = function(packet) {
+    m
+      .request({
+        url: packet.url,
+        method: "GET",
+        dataType: "jsonp",
+        callbackKey: "jsonp",
+        data: {
+          message: packet.body
+        },
+        config: function(xhr) {
+          _setHeaders(xhr, packet.headers);
+        }
+      })
+      .then(packet.onSuccess, packet.onError);
+  };
 
-    return that;
+  return that;
 }
 
 Cometd = function(name) {
-    var cometd = new org_cometd.CometD(name);
+  var cometd = new org_cometd.CometD(name);
 
-    if (org_cometd.WebSocket) {
-        cometd.registerTransport('websocket', new org_cometd.WebSocketTransport());
-    }
+  if (org_cometd.WebSocket) {
+    cometd.registerTransport("websocket", new org_cometd.WebSocketTransport());
+  }
 
-    cometd.registerTransport('long-polling', new LongPollingTransport());
-    cometd.registerTransport('callback-polling', new CallbackPollingTransport());
+  cometd.registerTransport("long-polling", new LongPollingTransport());
+  cometd.registerTransport("callback-polling", new CallbackPollingTransport());
 
-    return cometd;
+  return cometd;
+};
+
+function attempt(fn) {
+  try {
+    fn();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = Cometd;
